d726a8bd5d8ff8bb003f200247296d3708408140
Bernard Anderson <bernard.anderson@gmail.com>
2016-03-10 22:34:34 -0600

    added the DOM elements

--- weathercode.js
+++ weathercode.js
@@ -1,19 +1,15 @@
-var parsedWeatherObject;
-
+// Parses the Json WeatherObject
 function parseWeather() {
-  // var parsedWeatherObject = this.responseText;
-  parsedWeatherObject = JSON.parse(this.responseText);
-  console.log("parsedWeatherObject", parsedWeatherObject);
+  var parsedWeatherObject = JSON.parse(this.responseText);
 }
 
+// Pulls the json'd weather info from the Weather API.
 function XHRWeather() {
-
   var requestOpenWeather = new XMLHttpRequest();
   requestOpenWeather.addEventListener("load", parseWeather);
   requestOpenWeather.open("GET", "weather.json");
   // requestOpenWeather.open("GET", "http://api.openweathermap.org/data/2.5/weather?zip=37221,us&APPID=b65ed22c41380f0f6c0e50fae7874970");
   requestOpenWeather.send();
-
 }
 
 XHRWeather();
