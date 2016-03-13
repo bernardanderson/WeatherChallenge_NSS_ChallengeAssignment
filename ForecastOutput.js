38dcb05b16aeaf86540bc80e744f06fd5d996186
Bernard Anderson <bernard.anderson@gmail.com>
2016-03-13 15:40:04 -0500

    added formatting for weather output and css colors to the background

--- ForecastOutput.js
+++ ForecastOutput.js
@@ -5,24 +5,50 @@ var ForecastOutput = (function() {
 
   return {
 
+    addColorToDayDiv: function(sentSkyCondition) {
+      var skyType = ["Snow", "Rain", "Clouds"]
+
+      for (var i = 0; i < skyType.length; i++) {
+        if (sentSkyCondition === skyType[i]){
+          return "sky-color-gray";
+        };
+      };
+
+      return "sky-color-blue"
+      // return "#50D0FF"; // Nice sky blue color
+
+    },
+
     addWeatherToDivs: function() {
       for (var i = 0; i < sntWeatObj.list.length; i++) {
         var currentDayDiv = document.getElementById(`day-${i}`);
         var weatherString = `<p class="day">${ForecastOutput.timeToHuman(i)}</p>`;
-        weatherString += `<p class="description">Weather: ${sntWeatObj.list[i].weather[0].description}</p>`;
+        
+        weatherString += `<p class="description">Weather: ${ForecastOutput.formatSkyConditions(sntWeatObj.list[i].weather[0].description)}</p>`;
         
         var dayTemp = ForecastOutput.tempToFahrenheit(sntWeatObj.list[i].temp.day);
         var nightTemp = ForecastOutput.tempToFahrenheit(sntWeatObj.list[i].temp.night);
-        weatherString += `<p class="temps">Hi:${dayTemp} / Low:${nightTemp}&degF</p>`;
+        weatherString += `<p class="temps">Hi: ${dayTemp} / Low: ${nightTemp}&degF</p>`;
         currentDayDiv.innerHTML = weatherString;
+
+        currentDayDiv.classList.add(ForecastOutput.addColorToDayDiv(sntWeatObj.list[i].weather[0].main));
+        // currentDayDiv.classList.add = divBgColorClass;
       };
     },
+
+// This capitalizes the first letter in the detailed conditions report
+    formatSkyConditions: function(sentDetailedSkyCondition) {
+      var capitalizedSkyConditions = sentDetailedSkyCondition.split(" ").map(function(arrayItem) {
+        return (arrayItem.charAt(0).toUpperCase() + arrayItem.substring(1));
+      }).join(" ");
+      return capitalizedSkyConditions;
+    },
+
 
 // This builds the holders for the weather output on the DOM
     generalContentDisplay: function(sentParsedWeatherObject) {
 
       sntWeatObj = sentParsedWeatherObject;
-      console.log(sntWeatObj);
 
       mainWeatherOutputDiv.innerHTML = `<p class=headline>Here's your ${sntWeatObj.cnt} day forecast for ${sntWeatObj.city.name}, ${sntWeatObj.city.country}...</p>`;
 
@@ -41,19 +67,40 @@ var ForecastOutput = (function() {
       ForecastOutput.addWeatherToDivs();
     },
 
+// This converts the openweather temp (in K) to fahrenheit (F)
+    tempToFahrenheit: function(sentKelvinTemp) {
+      var fahrenheitTemp = Math.round((sentKelvinTemp - 273.15) * 1.8 + 32);
+      return fahrenheitTemp;
+    },
+
 // This converts the UNIX time into human readable time
     timeToHuman: function(sentCurrentDay) {
       var humanDate = new Date(sntWeatObj.list[sentCurrentDay].dt * 1000);
       var dateArray = humanDate.toGMTString().split(" ");
-      var monthDay = `${dateArray[0].substring(0, 3)}, ${dateArray[2]} ${dateArray[1]}`;
+
+// This object is used to convert the three char day into the full name
+      var dayAbbrObject = 
+        { 
+          "Mon": "Monday",
+          "Tue": "Tuesday",
+          "Wed": "Wednesday",
+          "Thu": "Thursday",
+          "Fri": "Friday",
+          "Sat": "Saturday",
+          "Sun": "Sunday"
+        };
+
+      var tempDateAbriv = dateArray[0].substring(0, 3);
+      for (day in dayAbbrObject) {
+        if (day === tempDateAbriv) {
+          dateArray[0] = dayAbbrObject[day];
+        };
+      };
+
+      var monthDay = `${dateArray[0]}, ${dateArray[2]} ${dateArray[1]}`;
+
       return monthDay;
-    },
-
-    tempToFahrenheit: function(sentKelvinTemp) {
-      var fahrenheitTemp = Math.round((sentKelvinTemp - 273.15) * 1.8 + 32);
-      return fahrenheitTemp;
     }
-
   };
 
 })(ForecastOutput || {});
